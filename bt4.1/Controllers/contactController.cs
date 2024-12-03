43c8d49e0639674948c36afa13a2378be920c8d5
Bao Gia <nguyenducgiabao18@outlook.com>
2024-12-03 08:03:02 +0700

    Add project files.

---
+++ bt4.1/Controllers/contactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bt4._1.Controllers
+{
+    public class contactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
